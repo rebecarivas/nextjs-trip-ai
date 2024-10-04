b3ae66300f62de51fe92078c281b67782b378c8a
rebecarivas <rebecarivas@gmail.com>
2024-10-04 15:35:50 -0300

    add styled-components configuration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
+import StyledComponentsRegistry from "../../lib/registry.js";
+import GlobalStyle from "../../styles/global.js";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="pt-br">
+        <body>
+          <GlobalStyle />
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </body>
+      </html>
+    </>
   );
 }
